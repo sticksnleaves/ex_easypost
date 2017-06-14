b60e3e737ed794585881174066bba8a4a3ebb90f
Anthony Smith <anthony@sticksnleaves.com>
2017-06-14 13:00:02 -0400

    First!

---
+++ lib/ex_easypost.ex
@@ -0,0 +1,18 @@
+defmodule ExEasyPost do
+  @moduledoc """
+  Documentation for ExEasyPost.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExEasyPost.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_easypost_test.exs
@@ -0,0 +1,8 @@
+defmodule ExEasyPostTest do
+  use ExUnit.Case
+  doctest ExEasyPost
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
