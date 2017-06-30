58c45fe3ce66dfffddd1cb2d95d5cd36c65380c1
Anthony Smith <anthony@sticksnleaves.com>
2017-06-30 10:16:27 -0400

    Added support for the batch endpoints

---
+++ lib/ex_easypost/resource/batch.ex
@@ -0,0 +1,28 @@
+defmodule ExEasyPost.Batch do
+  use ExEasyPost.Resource, import: [
+    :create
+  ]
+
+  @spec add_shipments(binary, [map]) :: ExEasyPost.Operation.t
+  def add_shipments(id, shipments) do
+    request(:post, "batches/#{id}/add_shipments", shipments)
+  end
+
+  @spec label(binary, map) :: ExEasyPost.Operation.t
+  def label(id, params) do
+    request(:post, "batches/#{id}/label", params)
+  end
+
+  @spec remove_shipments(binary, [map]) :: ExEasyPost.Operation.t
+  def remove_shipments(id, shipments) do
+    request(:post, "batches/#{id}/remove_shipments", shipments)
+  end
+
+  @spec scan_form(binary) :: ExEasyPost.Operation.t
+  def scan_form(id) do
+    request(:post, "batches/#{id}/scan_form")
+  end
+
+  @doc false
+  def create_url, do: "batches"
+end
---
+++ test/lib/ex_easypost/resource/batch_test.exs
@@ -0,0 +1,51 @@
+defmodule ExEasyPost.BatchTest do
+  use ExUnit.Case, async: true
+
+  describe "add_shipments/2" do
+    test "builds an operation" do
+      id = "foo"
+      params = [%{foo: "bar"}]
+
+      assert %ExEasyPost.Operation{http_method: :post, params: ^params, path: "batches/foo/add_shipments"}
+        = ExEasyPost.Batch.add_shipments(id, params)
+    end
+  end
+
+  describe "create/1" do
+    test "builds an operation" do
+      params = %{foo: "bar"}
+
+      assert %ExEasyPost.Operation{http_method: :post, params: ^params, path: "batches"}
+        = ExEasyPost.Batch.create(params)
+    end
+  end
+
+  describe "label/2" do
+    test "builds an operation" do
+      id = "foo"
+      params = %{foo: "bar"}
+
+      assert %ExEasyPost.Operation{http_method: :post, params: ^params, path: "batches/foo/label"}
+        = ExEasyPost.Batch.label(id, params)
+    end
+  end
+
+  describe "remove_shipments/2" do
+    test "builds an operation" do
+      id = "foo"
+      params = [%{foo: "bar"}]
+
+      assert %ExEasyPost.Operation{http_method: :post, params: ^params, path: "batches/foo/remove_shipments"}
+        = ExEasyPost.Batch.remove_shipments(id, params)
+    end
+  end
+
+  describe "scan_form/1" do
+    test "builds an operation" do
+      id = "foo"
+
+      assert %ExEasyPost.Operation{http_method: :post, path: "batches/foo/scan_form"}
+        = ExEasyPost.Batch.scan_form(id)
+    end
+  end
+end
