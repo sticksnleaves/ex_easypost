24c4e00ec2c3bd5318e180d1b067dec3bd6bf1b7
Anthony Smith <anthony@sticksnleaves.com>
2017-11-25 23:33:36 -0500

    Added support for the batch resource

--- test/lib/ex_easypost/batch_test.exs
+++ test/lib/ex_easypost/batch_test.exs
@@ -4,18 +4,18 @@ defmodule ExEasyPost.BatchTest do
   describe "add_shipments/2" do
     test "builds an operation" do
       id = "foo"
-      params = [%{foo: "bar"}]
+      params = [%{ foo: "bar" }]
 
-      assert %ExEasyPost.Operation{http_method: :post, params: ^params, path: "batches/foo/add_shipments"}
+      assert %ExEasyPost.Operation{ http_method: :post, params: %{ batch: %{ shipments: ^params } }, path: "batches/foo/add_shipments" }
         = ExEasyPost.Batch.add_shipments(id, params)
     end
   end
 
   describe "create/1" do
     test "builds an operation" do
-      params = %{foo: "bar"}
+      params = [%{ foo: "bar" }]
 
-      assert %ExEasyPost.Operation{http_method: :post, params: ^params, path: "batches"}
+      assert %ExEasyPost.Operation{ http_method: :post, params: %{ batch: %{ shipments: ^params } }, path: "batches" }
         = ExEasyPost.Batch.create(params)
     end
   end
@@ -23,9 +23,9 @@ defmodule ExEasyPost.BatchTest do
   describe "label/2" do
     test "builds an operation" do
       id = "foo"
-      params = %{foo: "bar"}
+      params = %{ foo: "bar" }
 
-      assert %ExEasyPost.Operation{http_method: :post, params: ^params, path: "batches/foo/label"}
+      assert %ExEasyPost.Operation{ http_method: :post, params: ^params, path: "batches/foo/label" }
         = ExEasyPost.Batch.label(id, params)
     end
   end
@@ -33,9 +33,9 @@ defmodule ExEasyPost.BatchTest do
   describe "remove_shipments/2" do
     test "builds an operation" do
       id = "foo"
-      params = [%{foo: "bar"}]
+      params = [%{ foo: "bar" }]
 
-      assert %ExEasyPost.Operation{http_method: :post, params: ^params, path: "batches/foo/remove_shipments"}
+      assert %ExEasyPost.Operation{ http_method: :post, params: %{ batch: %{ shipments: ^params } }, path: "batches/foo/remove_shipments" }
         = ExEasyPost.Batch.remove_shipments(id, params)
     end
   end
@@ -44,7 +44,7 @@ defmodule ExEasyPost.BatchTest do
     test "builds an operation" do
       id = "foo"
 
-      assert %ExEasyPost.Operation{http_method: :post, path: "batches/foo/scan_form"}
+      assert %ExEasyPost.Operation{ http_method: :post, path: "batches/foo/scan_form" }
         = ExEasyPost.Batch.scan_form(id)
     end
   end
