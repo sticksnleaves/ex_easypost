b8fac9aefbdac643266de6e81b7f876e605e0a78
Anthony Smith <anthony@sticksnleaves.com>
2017-11-25 21:54:54 -0500

    Moved all response handling logic into ExEasyPost.Operation

--- lib/ex_easypost/client.ex
+++ lib/ex_easypost/client.ex
@@ -3,20 +3,9 @@ defmodule ExEasyPost.Client do
 
   def request(http_method, url, params, headers, config) do
     all_headers = build_headers(headers, config)
-    client = config[:http_client]
-    parser = config[:json_parser]
 
-    case client.request(http_method, url, parser.encode!(params), all_headers, config[:http_opts]) do
-      {:ok, response = %{status_code: status_code}} when status_code in 200..299 ->
-        {:ok, response}
-      {:ok, %{body: body, status_code: status_code}} when status_code >= 400 ->
-        {:error, {:http_error, status_code, parser.decode!(body)}}
-      {:error, %{reason: reason}} ->
-        {:error, {:client_error, reason}}
-    end
+    config[:http_client].request(http_method, url, params, all_headers, config[:http_opts])
   end
-
-  # private
 
   defp build_headers(headers, config) do
     headers ++ [
