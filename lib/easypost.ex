3be1e089e7d788262f26c415a684588c26dba4f9
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-05-04 13:40:07 -0400

    Added response type

--- lib/easypost.ex
+++ lib/easypost.ex
@@ -2,4 +2,6 @@ defmodule EasyPost do
   @type http_headers_t :: [{ String.t(), String.t() }]
 
   @type http_method_t :: :delete | :get | :post | :put
+
+  @type response_t :: { :ok, Response.t() } | { :error, Response.t() | any }
 end
