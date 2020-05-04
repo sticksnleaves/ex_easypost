f84c5c94d537e9907b2b53c541c8c3759c198112
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-05-04 13:46:16 -0400

    Fixed type response_t

--- lib/easypost.ex
+++ lib/easypost.ex
@@ -1,7 +1,10 @@
 defmodule EasyPost do
-  @type http_headers_t :: [{ String.t(), String.t() }]
+  @type http_headers_t ::
+          [{ String.t(), String.t() }]
 
-  @type http_method_t :: :delete | :get | :post | :put
+  @type http_method_t ::
+          :delete | :get | :post | :put
 
-  @type response_t :: { :ok, Response.t() } | { :error, Response.t() | any }
+  @type response_t ::
+          { :ok, EasyPost.Response.t() } | { :error, EasyPost.Response.t() | any }
 end
