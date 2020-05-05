a8c8a1bdb69fda25933b9209120d5aaca3d2a056
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-05-05 15:33:14 -0400

    Added public function for making a request

--- lib/easypost.ex
+++ lib/easypost.ex
@@ -1,4 +1,6 @@
 defmodule EasyPost do
+  alias EasyPost.{ Config, Request }
+
   @type http_headers_t ::
           [{ String.t(), String.t() }]
 
@@ -7,4 +9,12 @@ defmodule EasyPost do
 
   @type response_t ::
           { :ok, EasyPost.Response.t() } | { :error, EasyPost.Response.t() | any }
+
+  @doc """
+  Send an HTTP request to the EasyPost API.
+  """
+  @spec request(EasyPost.Operation.t(), EasyPost.Config.t()) :: response_t
+  def request(operation, config) do
+    Request.send(operation, Config.new(config))
+  end
 end
