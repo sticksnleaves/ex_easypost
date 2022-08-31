cc32f767faf51a15d87af4bae9f84ec1c56e83e5
Anthony Smith <420061+anthonator@users.noreply.github.com>
2022-08-31 15:26:41 -0400

    Bumped version to 3.0.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EasyPost.MixProject do
   def project do
     [
       app: :ex_easypost,
-      version: "3.0.3",
+      version: "3.0.4",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
